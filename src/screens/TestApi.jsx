8dfe54803002afc22632c5d0e5e088c613a1cafa
roob1y <robwork122@gmail.com>
2022-11-20 19:04:24 +0000

    cleanup code

--- src/screens/TestApi.jsx
+++ src/screens/TestApi.jsx
@@ -6,39 +6,41 @@
 
 import React, { useState, useEffect, useRef } from "react";
 import MapView, { Marker, Callout } from "react-native-maps";
-import { StyleSheet, Text, View, Dimensions, Button } from "react-native";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import { Button } from "react-native-paper";
 import * as Location from "expo-location";
-import customMapStyle from "../mapStyle.json"
+import customMapStyle from "../mapStyle.json";
 
 function TestApi() {
   const mapRef = useRef(null);
-  const [region, setRegion] = useState({
+
+  const [location, setLocation] = useState({
     latitude: 37.78825,
     longitude: -122.4324,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   });
 
-  const tokyoRegion = {
-    latitude: 35.6762,
-    longitude: 139.6503,
-    latitudeDelta: 0.01,
-    longitudeDelta: 0.01,
-  };
+  const [isLoading, setIsLoading] = useState(false)
+
+  const [currLocation, setCurrLocationRegion] = useState(null);
 
   const [errorMsg, setErrorMsg] = useState(null);
 
   function userLocation() {
+    setIsLoading(true);
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== "granted") {
         setErrorMsg("Permission to access location was denied");
+        setIsLoading(false);
         return;
       }
       let location = await Location.getCurrentPositionAsync({
         enableHighAccuracy: true,
       });
-      setRegion({
+      setIsLoading(false);
+      setCurrLocationRegion({
         latitude: location.coords.latitude,
         longitude: location.coords.longitude,
         latitudeDelta: 0.0022,
@@ -47,40 +49,41 @@ function TestApi() {
     })();
   }
 
-  // let text = "Waiting..";
-  // if (errorMsg) {
-  //   text = errorMsg;
-  // } else if (location) {
-  //   text = JSON.stringify(location);
-  // }  
-
-  const goToTokyo = () => {
-    //complete this animation in 3 seconds
-    mapRef.current.animateToRegion(tokyoRegion, 3 * 1000);
-  };
+  
 
   const goToLocation = () => {
     //Animate the user to new region. Complete this animation in 3 seconds
-    mapRef.current.animateToRegion(region, 3 * 1000);
+    userLocation()
+    mapRef.current.animateToRegion(currLocation, 3 * 1000);
   };
 
   return (
     <View style={styles.container}>
       <MapView
         style={styles.map}
-        region={region}
+        initialRegion={location}
         customMapStyle={customMapStyle}
         ref={mapRef}
         provider="google"
+        showsUserLocation={true}
       >
-        <Marker coordinate={region} title="Marker">
+        {/* <Marker title="Marker">
           <Callout>
             <Text>Location</Text>
           </Callout>
-        </Marker>
+        </Marker> */}
       </MapView>
-      <Button onPress={() => goToTokyo()} title="Go to Tokyo" />
-      <Button title="Get Location" onPress={() => userLocation()}></Button>
+      <Button
+        style={styles.button}
+        isLoading={true}
+        onPress={() => goToLocation()}
+        icon="arrow-right"
+				mode="contained"
+				contentStyle={{ height: 50, flexDirection: "row-reverse" }}
+				loading={isLoading}
+      >
+        Get Current Location
+      </Button>
     </View>
   );
 }
