8866ee83d9ad8a02dce7baf7dcc2bc13ed645872
roob1y <robwork122@gmail.com>
2022-11-21 12:30:24 +0000

    added input field to autocomplete page

--- src/screens/AutoCompleteAddresses.jsx
+++ src/screens/AutoCompleteAddresses.jsx
@@ -1,7 +1,41 @@
-import { Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { TextInput, Button } from "react-native-paper";
 
 function AutoCompleteAddresses() {
-  return <Text>autocomplete</Text>;
+  return (
+    <SafeAreaView>
+      <Text>autocomplete</Text>
+      <View style={styles.autocompleteContainer}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            
+          ></TextInput>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  autocompleteContainer: {
+    marginBottom: 20,
+    // padding: 20,
+    // flex: 1,
+    // backgroundColor: "gray",
+  },
+  inputContainer: {
+  },
+  button: {
+  }
+  // inputContainerInput: {
+  //   flex: 1,
+  //   outline: none,
+  //   border: 1 solid rgba(0, 0, 0, 0.2),
+  //   padding: 10,
+  //   padding-right: 31,
+  //   font-size: 16,
+  // }
+});
 
 export default AutoCompleteAddresses;
