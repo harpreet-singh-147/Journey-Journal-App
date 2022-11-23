efa9fdf609cab395a4b36016344adf7eb6ff2d48
harpreet-singh-147 <h_singh_007@hotmail.co.uk>
2022-11-23 22:19:31 +0000

    homepage styling done

--- src/screens/HomePage.jsx
+++ src/screens/HomePage.jsx
@@ -1,41 +1,38 @@
-import React, { useState } from "react";
+import React from "react";
 import { StyleSheet, Text, View, ImageBackground } from "react-native";
 import { useAuthentication } from "../../utils/hooks/userAuthentication";
-import { Button } from "react-native-paper";
-import { getAuth, signOut } from "firebase/auth";
-
-const auth = getAuth();
-
-const user = auth.currentUser;
+import { useFonts, Poppins_600SemiBold } from "@expo-google-fonts/poppins";
 
 export default function HomeScreen() {
   const { user } = useAuthentication();
-  const [signOutIsLoadingBtn, setSignOutIsLoadingBtn] = useState(false);
+
+  let [fontsLoaded] = useFonts({
+    Poppins_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   const image = {
     uri: "https://images.unsplash.com/photo-1501868984184-76121ed6a6e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2970&q=80",
   };
 
-  // function signOutBtnFuncCombine() {
-  //   setSignOutIsLoadingBtn(true);
-  //   signOut(auth);
-  // }
-
   return (
     <View style={styles.container}>
       <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-        <View style={{ flex: 1, marginTop: 390, padding: 5 }}>
+        <View style={{ flex: 1, marginTop: 470, padding: 5 }}>
           <Text
             style={{
-              // marginTop: ,
-              fontSize: 25,
+              fontSize: 17,
               fontWeight: "600",
-              // alignSelf: "center",
-              // marginHorizontal: 10,
               textAlign: "center",
-              color: "white",
-              backgroundColor: "rgba(0,0,0,0.7)",
+              color: "#E7F6F2",
+              backgroundColor: "#000000",
+              opacity: 0.9,
               borderRadius: 25,
+              fontFamily: "Poppins_600SemiBold",
+              padding: 5,
             }}
           >
             Welcome {user?.displayName}! Are you planning a trip sometime soon?
@@ -45,17 +42,6 @@ export default function HomeScreen() {
           </Text>
         </View>
       </ImageBackground>
-
-      {/* <Button
-        style={styles.button}
-        onPress={() => signOutBtnFuncCombine()}
-        icon="arrow-right"
-        mode="contained"
-        contentStyle={{ height: 50, flexDirection: "row-reverse" }}
-        loading={signOutIsLoadingBtn}
-      >
-        Sign Out
-      </Button> */}
     </View>
   );
 }
